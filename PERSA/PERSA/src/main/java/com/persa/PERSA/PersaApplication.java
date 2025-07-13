f0e82216903275627e7eac54ed84779b321c9def
Juan Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-07-13 10:51:00 -0500

    Inicio de Persa
    Se comenzó el proyecto persa con SpringBoot

---
+++ PERSA/PERSA/src/main/java/com/persa/PERSA/PersaApplication.java
@@ -0,0 +1,13 @@
+package com.persa.PERSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersaApplication.class, args);
+	}
+
+}
