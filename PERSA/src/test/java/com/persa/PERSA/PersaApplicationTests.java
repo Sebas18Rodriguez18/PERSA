4232c7f1114b4bd78fa5ccd842ac84b3739c2991
Juan Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-07-31 23:55:25 -0500

    Inicio
    Se realizó el comienzo de PERSA en SpringBoot, al igual que la base de bootstrap ARGON

---
+++ PERSA/src/test/java/com/persa/PERSA/PersaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persa.PERSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
