baeb54ae0c94ad647152020be956e88d97986fbe
Juan Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-09-09 18:53:48 -0500

    Creación de SpringBoot
    Se creó el proyecto springboot con todas la dependencias necesarias.

---
+++ persa-react/persa/src/main/java/co/edu/sena/persa/PersaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.sena.persa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersaApplication.class, args);
+	}
+
+}
