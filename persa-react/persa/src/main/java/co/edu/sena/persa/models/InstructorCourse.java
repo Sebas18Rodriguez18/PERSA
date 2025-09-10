c58b3891e76c4be6af7392c1c87f93f3087bcc9c
Juan Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-09-09 19:51:23 -0500

    Creación de entidades puente
    FEAT(models y repositories): se crearon las entidades JPA según la base de datos PERSA de las tablas puente.

---
+++ persa-react/persa/src/main/java/co/edu/sena/persa/models/InstructorCourse.java
@@ -0,0 +1,40 @@
+package co.edu.sena.persa.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "instructor_course")
+public class InstructorCourse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "instructor_id", nullable = false)
+    private User instructor;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+}
